d4fd6a192cd169c1d679ceed174cdefdbb941fa8
Jitesh Kumar <Jitesh.Kumar@nice.com>
2020-02-23 11:24:22 +0530

    Adding StockTrader full Project

---
+++ src/components/Portfolio/Portfolio.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+        <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+    </div>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+import Stock from './Stock.vue';
+
+export default {
+    computed: {
+        ...mapGetters({
+        stocks: 'stockPortfolio'
+    })
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
